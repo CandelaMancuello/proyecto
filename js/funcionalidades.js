54940e5bab1a8bec972b946128682491ba14f633
Candela Mancuello <delalaura98@mail.com>
2024-10-28 09:11:10 -0300

    agrege javascript

---
+++ js/funcionalidades.js
@@ -0,0 +1,27 @@
+const audioPlayerUno = document.getElementById('audioPlayer');
+const buttonUno= document.getElementById('buttonUno');
+
+buttonUno.addEventListener('click', function() {
+     audioPlayerUno.play(); 
+});
+const audioPlayerDos = document.getElementById('audioPlayer');
+const buttonDos= document.getElementById('button');
+
+button.addEventListener('click', function() {
+     audioPlayerDos.play(); 
+});
+
+const audioPlayerTres = document.getElementById('audioPlayer');
+const buttonTres= document.getElementById('button');
+
+button.addEventListener('click', function() {
+     audioPlayerTres.play(); 
+});
+const audioPlayerCuatro = document.getElementById('audioPlayer');
+const buttonCuatro= document.getElementById('button');
+
+button.addEventListener('click', function() {
+     audioPlayerCuatro.play(); 
+});
+
+
